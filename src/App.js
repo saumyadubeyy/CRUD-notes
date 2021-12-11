0b96cbf1f5ec3e6f2c3573555a5f531fa9a417bd
Saumya Dubey <saumyadb29@gmail.com>
2021-12-11 23:34:56 +0530

    auth-check

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import HomePage from './components/HomePage'
-import Navbar from './components/Navbar'
 import Footer from './components/Footer'
+import firebase from "./service/firebase"
 
 const App = () => {
+  const [user,setUser] = useState(" ")
+
+    useEffect(() => {
+        firebase.auth().onAuthStateChanged(user => {
+            setUser(user);
+        })
+    }, [])
+
   return (
     <div>
-      <Navbar />
-      <HomePage />
+      <HomePage user={user} />
       <Footer />
     </div>
   )
--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -7,8 +7,10 @@ import Notes from './Notes'
 import inputActions from '../redux/actions/inputActions'
 import Modal from 'react-modal'
 import img from '../assets/main.png'
+import { auth } from "../service/firebase"
+import { signInWithGoogle } from "../service/firebase";
 
-const HomePage = () => {
+const HomePage = ({user}) => {
   const dispatch = useDispatch();
   const display = useSelector(state => state.formDisplay)
 
@@ -19,24 +21,48 @@ const HomePage = () => {
 
     return (
         <div>
-            <div className="img">
-                <img src={img} alt='' className="img" />
-            </div>
-              <Modal 
-                  isOpen={display} 
-                  onRequestClose={newNote}
-                  shouldCloseOnEsc={false}
-                  shouldCloseOnOverlayClick={false}
-                  className="content animate-modal"
-                  overlayClassName="overlay"
-              >
-              <button onClick={newNote} className="cross top">X</button>
-                  <CreateNote  />
-              </Modal>
-              <Notes />
-              <div>
-                <button onClick={newNote} className="plus">New note</button>
-              </div>
+          {
+            user ?
+              <div> 
+                <nav>
+                    {user ? 
+                        <h4 className='name'>Welcome, {user.displayName} </h4>: <h5>ssup</h5>}
+                        <div className="nav">
+                          {user ? <h4 className="signOut" onClick={() => auth.signOut()}>Sign Out</h4>: <h4></h4>}
+                      </div>
+                </nav>
+                <div className="img">
+                    <img src={img} alt='' className="img" />
+                </div>
+                <Modal 
+                    isOpen={display} 
+                    onRequestClose={newNote}
+                    shouldCloseOnEsc={false}
+                    shouldCloseOnOverlayClick={false}
+                    className="content animate-modal"
+                    overlayClassName="overlay"
+                >
+                <button onClick={newNote} className="cross top">X</button>
+                    <CreateNote  />
+                </Modal>
+                <Notes />
+                <div>
+                    <button onClick={newNote} className="plus">New note</button>
+                </div> 
+              </div> 
+              : 
+              <div className='else'>
+                  
+                  <h4 className="greet">Hello, welcome <br/>You can create, display, modify and delete your notes using this web-app.<br/>Sign in right now, and start creating :)</h4>        
+                  <div className="signIn" onClick={signInWithGoogle}>Sign In</div>
+
+                  <div className="img">
+                      <img src={img} alt='' className="img" />
+                  </div>
+                  <div className='ps'>PS. I'm still learning how to add database to one's application using Firestore Database.<br/>Therfore, as of now, there is no database linked to this application, so do not save important data which you would like to retrieve later in your notes. <br/>Thank you :)</div>
+                </div>
+          }
+            
         </div>
     )
 }
